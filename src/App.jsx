6c204789ec1462a61b21f97c88c9568917ca6b17
cristobalhiza <cristobalhiza@gmail.com>
2024-05-20 19:14:25 -0400

    inicia proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NavBarComponent from './components/NavBarComponent/NavBarComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+NavBarComponent
+
+function App() {
+
+    return (
+        <>
+        <NavBarComponent/>
+        </>
+    )
+};
+
+
+export default App
